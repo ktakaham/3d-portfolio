b5d1f7dfefb2a9d419ab539f950b2333ed8aa68e
hummar <112448683+ktakahama@users.noreply.github.com>
2023-02-19 21:50:29 +0900

    追加

--- src/components/pages/portfolio/PortfolioPageComponent.tsx
+++ src/components/pages/portfolio/PortfolioPageComponent.tsx
@@ -4,6 +4,16 @@ import Spline from "@splinetool/react-spline";
 
 const links = [
   {
+    url: "https://prod.spline.design/xSfN9Hu5MfehHefI/scene.splinecode",
+    title: "UP POPUP STORE HARAJYUKU",
+    date: "2023/02/19",
+  },
+  {
+    url: "https://prod.spline.design/FuVrfouZlXLGN62I/scene.splinecode",
+    title: "原宿駅前のロードバイク",
+    date: "2023/02/19",
+  },
+  {
     url: "https://prod.spline.design/2KYzpJ1RhNmS-5yF/scene.splinecode",
     title: "Birthday Cake 2023",
     date: "2023/2/18",
